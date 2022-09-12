01035386fa0dabc2ed1583719801b0ac530663e9
da-head0 <andyourturntome@gmail.com>
2022-09-13 00:56:45 +0900

    fix:#35:fix Dockerfile and env

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ${{ secrets.SECRET_KEY }} 
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ${{ secrets.DEBUG }} 
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = ['*']
 
