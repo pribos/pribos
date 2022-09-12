ec2afb64cf573989301844d39476c3c3e2dceb44
Dahye <61692777+da-head0@users.noreply.github.com>
2022-09-12 21:17:57 +0900

    35 (#43)
    * fix:#35:fix os.environ
    
    * fix:#35:fix env
    
    * fix:#35:fix env2
    
    * fix:#35:fix github secret
    
    * fix:#35:fix env2
    
    * fix:#35:fix github secret

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ["GITHUB_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = ['*']
 
@@ -111,11 +111,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
